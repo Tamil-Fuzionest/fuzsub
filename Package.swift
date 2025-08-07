c809e7eabbc647c56dd94593f692ed89134bf056
TamilOfficial <74907995+Tamil-Fuzionest@users.noreply.github.com>
2025-08-08 00:10:36 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
    ],
    targets: [
       .binaryTarget(
-         name: "SubscriptionSDKKit",
+         name: "FuzSub",
          url: "https://github.com/Tamil-Fuzionest/subscription_sdk/releases/download/v0.0.1/FuzSub.xcframework.zip",
          checksum:"3d2ea7aa69c3f52cc163ff2154b808b1cb6ce5a0014ff30445ca0ae4202c5e83")
    ]
